f103baac3404cfd0d5ed1c9ebb9965877bedc612
SelvaganapathySakthivel <selvaganapathy.sakthivel@gmail.com>
2025-05-04 09:24:56 -0700

    new module created

---
+++ math.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b
+}
+function mult(a,b){
+    return a*b
+}
+function divt(a,b){
+return a/b
+}
+export{
+    add,sub,mult,divt
+}
